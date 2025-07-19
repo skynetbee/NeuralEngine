934f4581940e09c7fa8bc91a10e23177ac4eb4d7
Monish <monish-instinct@users.noreply.github.com>
2025-07-19 14:38:49 +0530

    Add NeuralEngine.xcframework to Frameworks folder

---
+++ Frameworks/NeuralEngine.xcframework/ios-arm64/NeuralEngine.framework/Headers/NeuralEngine.h
@@ -0,0 +1,18 @@
+//
+//  NeuralEngine.h
+//  NeuralEngine
+//
+//  Created by Skynet Studios on 19/07/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NeuralEngine.
+FOUNDATION_EXPORT double NeuralEngineVersionNumber;
+
+//! Project version string for NeuralEngine.
+FOUNDATION_EXPORT const unsigned char NeuralEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeuralEngine/PublicHeader.h>
+
+
